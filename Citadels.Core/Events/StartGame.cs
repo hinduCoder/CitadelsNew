f60bd687f08960c2c624767c06810aa5fcf457ee
Anton <gribov_an@astralnalog.ru>
2023-02-17 17:17:25 +0300

    fix StartGame event

--- Citadels.Core/Events/StartGame.cs
+++ Citadels.Core/Events/StartGame.cs
@@ -6,17 +6,17 @@ namespace Citadels.Core.Events;
 public class StartGame : IGameEvent
 {
     private readonly List<string> _playersNames = new ();
+    private readonly List<Character> _randomizedCharacters = new();
+    private readonly List<District> _randomizedDistricts = new();
 
-    public List<Character> RandomizedCharacters { get; private set; }
+    public IReadOnlyList<string> PlayerNames => _playersNames;
+    public IReadOnlyList<Character> RandomizedCharacters => _randomizedCharacters;
 
-    public StartGame()
-    {
-        RandomizedCharacters = new(Character.Pool);
-        RandomizedCharacters.Shuffle();
-    }
     public StartGame(IEnumerable<string> playersNames)
-        :this()
     {
+        _randomizedCharacters.AddRange(Character.Pool);
+        _randomizedCharacters.Shuffle();
+
         _playersNames.AddRange(playersNames);
     }
 
@@ -29,4 +29,9 @@ public class StartGame : IGameEvent
         => game.Status == GameStatus.NotStarted
         && _playersNames.Count <= 7
         && _playersNames.Count >= 4; //special rules for two or three players are not going to be supported at first stage though we can allow to play them by regular rules
+
+    private StartGame()
+    {
+
+    }
 }
