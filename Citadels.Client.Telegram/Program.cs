68636177abbac8226b2d00c8cb1789f5f262bca3
Anton Gribov <gribov_an@astralnalog.ru>
2023-02-22 00:38:06 +0300

    added configuration, di, simple bot logic handling

--- Citadels.Client.Telegram/Program.cs
+++ Citadels.Client.Telegram/Program.cs
@@ -1,14 +1,27 @@
-﻿using Citadels.Client.Telegram.Resources;
+﻿using Citadels.Client.Telegram;
 using Microsoft.Extensions.Configuration;
-using System.Globalization;
+using Microsoft.Extensions.DependencyInjection;
 using System.Resources;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
 
 IConfiguration configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .AddEnvironmentVariables()
+    .AddUserSecrets("6501faf3-a56a-49ca-9573-5e5bf8e73409")
     .Build();
 
-var resourceManager = new ResourceManager(typeof(Strings));
-Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("en-US");
+var serviceProvider = new ServiceCollection()
+    .AddSingleton(new ResourceManager(typeof(Citadels.Client.Telegram.Resources.Strings)))
+    .AddSingleton<IUpdateHandler, TelegramUpdateHandler>()
+    .BuildServiceProvider();
 
-Console.WriteLine(resourceManager.GetString("Welcome"));
+var cancellationTokenSource = new CancellationTokenSource();
+
+Console.CancelKeyPress += (_, _) => cancellationTokenSource.Cancel();
+
+var token = configuration["Telegram:BotToken"]!;
+var telegramBot = new TelegramBotClient(token);
+telegramBot.StartReceiving(serviceProvider.GetRequiredService<IUpdateHandler>(), cancellationToken: cancellationTokenSource.Token);
+
+Console.ReadKey(); //don't know yet how to do better
---
+++ Citadels.Client.Telegram/TelegramUpdateHandler.cs
@@ -0,0 +1,27 @@
+﻿using Citadels.Client.Telegram.Resources;
+using System.Globalization;
+using System.Resources;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace Citadels.Client.Telegram;
+
+internal class TelegramUpdateHandler : IUpdateHandler
+{
+    public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        if (update.Message is null)
+        {
+            return;
+        }
+        var resourceManager = new ResourceManager(typeof(Strings));
+        Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(update.Message.From!.LanguageCode ?? "en-US");
+        await botClient.SendTextMessageAsync(update.Message.Chat.Id, resourceManager.GetString("Welcome")!);
+    }
+}
