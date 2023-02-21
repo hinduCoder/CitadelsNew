d07115a491757852134fc15bdb069cdad648c154
Anton Gribov <gribov_an@astralnalog.ru>
2023-02-22 00:05:14 +0300

    telegram client project created

---
+++ Citadels.Client.Telegram/Program.cs
@@ -0,0 +1,14 @@
+﻿using Citadels.Client.Telegram.Resources;
+using Microsoft.Extensions.Configuration;
+using System.Globalization;
+using System.Resources;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+var resourceManager = new ResourceManager(typeof(Strings));
+Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("en-US");
+
+Console.WriteLine(resourceManager.GetString("Welcome"));
