084c04d165e8e5ed935aa3ebb41264543c8ad41f
Anton Gribov <gribov_an@astralnalog.ru>
2023-02-22 19:51:03 +0300

    reorganized di for telegram client

--- Citadels.Client.Telegram/Program.cs
+++ Citadels.Client.Telegram/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
+using System;
 using System.Resources;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
@@ -21,7 +22,7 @@ var loggerConfiguration = new LoggerConfiguration()
 using var serviceProvider = new ServiceCollection()
     .AddSingleton(configuration)
     .AddSingleton(new ResourceManager(typeof(Citadels.Client.Telegram.Resources.Strings)))
-    .AddSingleton<IUpdateHandler, TelegramUpdateHandler>()
+    .AddScoped<IUpdateHandler, TelegramUpdateHandler>()
     .AddSingleton<ILogger>(loggerConfiguration.CreateLogger())
     .AddSingleton<ITelegramBotClient>(serviceProvider =>
     {
@@ -43,18 +44,27 @@ Console.CancelKeyPress += (_, e) =>
     e.Cancel = true;
 };
 
+var logger = serviceProvider.GetRequiredService<ILogger>();
+
+logger.Information("Started");
+
 var botInitializer = serviceProvider.GetRequiredService<TelegramBotSettingsInitializer>();
 await botInitializer.SetCommands();
 
+logger.Information("Bot initialization done");
+logger.Information("Starting listening");
+
 var telegramBot = serviceProvider.GetRequiredService<ITelegramBotClient>();
-telegramBot.StartReceiving(serviceProvider.GetRequiredService<IUpdateHandler>(), cancellationToken: cancellationTokenSource.Token);
-
-try
+await telegramBot.ReceiveAsync(async (t, u, ct) =>
 {
-    await Task.Delay(Timeout.Infinite, cancellationTokenSource.Token);
-} catch (TaskCanceledException)
+    using var scope = serviceProvider.CreateScope();
+    var updateHandler = scope.ServiceProvider.GetRequiredService<IUpdateHandler>();
+    await updateHandler.HandleUpdateAsync(t, u, ct);
+}, (_, ex, _) =>
 {
-}
+    logger.Error(ex, "Telegram update handler error");
+    return Task.CompletedTask;
+}, cancellationToken: cancellationTokenSource.Token);
 
-Console.WriteLine("Gracefull shutting down");
+logger.Information("Gracefull shutting down");
 await Task.Delay(5000);
